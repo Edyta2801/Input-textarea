a5e91750a4b7838d7e00ad44eb3374fad97f97c1
Edyta2801 <edyta.szarowska@interia.pl>
2020-11-07 09:07:32 +0100

    add style for App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-// import React from 'react';
+import React from 'react';
 import './App.css';
-import Button from './components/Button';
+import Button from './components/Button.js';
+
+const styles={
+  fontFamily: 'sans-serif',
+  marginLeft:100,
+  marginTop:60,
+}
+
 
 function App() {
   return (
-    <div>
-      <Button/>
+    <div style={styles}>
+      <Button color="amethyst" bgColor="midnightBlue" icon="smile">
+        Click me
+      </Button>
     </div>
   );
 }
