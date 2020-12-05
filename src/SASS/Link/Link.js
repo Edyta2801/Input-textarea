d5c131c9a7db154c4681efd7a7ef2e6f72dcf9be
Edyta2801 <edyta.szarowska@interia.pl>
2020-12-05 08:19:20 +0100

    Props style

--- src/SASS/Link/Link.js
+++ src/SASS/Link/Link.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+const primaryColor = '#900';
 
-
+const styles = {
+    color: primaryColor,
+    backgroundColor: 'white'
+};
 
 
 function Link() {
@@ -9,7 +13,7 @@ function Link() {
 
 
     return (
-        <a href='/'  >Name</a>
+        <a href='/' style={styles} >Name</a>
     )
 }
 export default Link;
